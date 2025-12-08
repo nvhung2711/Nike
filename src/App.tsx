f7a7d23b007a5173dbbf05125d5155fef4c7857a
nvhung2711 <nvhung2711@gmail.com>
2025-12-08 13:40:08 +1100

    create components for each section

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
+import {
+  Nav,
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subcribe,
+  Footer
+} from './sections'
+
 export default function App() {
 
   return (
     <>
-      <main className="relative">
-        Nav
-        <section className='xl:padding-l wide:padding-r padding-b'>
-          Hero
+      <main className="relative space-y-10">
+        <Nav />
+        <section className="xl:padding-l wide:padding-r padding-b">
+          <Hero />
         </section>
         <section className="padding">
-          PopularProducts
+          <PopularProducts />
         </section>
         <section className="padding">
-          SuperQuality
+          <SuperQuality />
         </section>
         <section className="padding-x py-10">
-          Services
+          <Services />
         </section>
         <section className="padding">
-          SpecialOffers
+          <SpecialOffer />
         </section>
         <section className="padding bg-pale-blue">
-          CustomerReviews
+          <CustomerReviews />
         </section>
         <section className="padding-x sm:py-32 py-16 w-full">
-          Subcribe
+          <Subcribe />
         </section>
         <section className="bg-black padding-x padding-t pb-8">
-          Footer
+          <Footer />
         </section>
       </main>
     </>
