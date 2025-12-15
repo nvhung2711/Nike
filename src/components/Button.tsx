ee96976ca2ff7921bbe1a9ee51a90cc27fc6dc7b
nvhung2711 <nvhung2711@gmail.com>
2025-12-15 13:04:01 +1100

    Customer Review section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,16 @@
-const Button = ({ label, iconURL }: { label: string, iconURL?: string }) => {
+type props = {
+    label: string,
+    iconURL?: string,
+    backgroundColor?: string,
+    textColor?: string,
+    borderColor?: string
+}
+
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }: props) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+        ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red text-white border-coral-red`}
+        `}>
             {label}
             {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
         </button>
